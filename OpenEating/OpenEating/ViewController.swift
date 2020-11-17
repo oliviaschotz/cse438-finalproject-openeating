db59cb7d1a2761b6d526ee121f790b93110ac389
oliviaschotz <oschotz@wustl.edu>
2020-11-17 14:25:28 -0600

    project

---
+++ OpenEating/OpenEating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenEating
+//
+//  Created by Olivia Schotz on 11/17/20.
+//  Copyright © 2020 Olivia Schotz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
