0fa3a08f9c29359ff3a04303a460ee0e5c0c679b
dsharabi <54600927+dsharabi@users.noreply.github.com>
2020-12-13 09:58:21 -0500

    wine pairing

---
+++ OpenEating/OpenEating/WineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WineViewController.swift
+//  OpenEating
+//
+//  Created by Danielle Sharabi on 12/13/20.
+//  Copyright © 2020 Olivia Schotz. All rights reserved.
+//
+
+import UIKit
+
+class WineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
